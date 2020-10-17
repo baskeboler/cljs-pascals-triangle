182eab876fea0d1283d770c2e59622c04a5428e0
Victor Gil <baskeboler@gmail.com>
2020-10-17 07:03:53 -0300

    refactored styles and implemented memoized pascal triangle line function

---
+++ src/main/pascal/core.cljs
@@ -0,0 +1,63 @@
+(ns pascal.core
+  (:require [reagent.core :as r]
+            [reagent.dom :refer [render]]
+            [pascal.styles :refer [css-str]]))
+
+
+(defonce app-root (. js/document (getElementById "root")))
+
+(def pascal-row-aux
+  (memoize
+   (fn [n]
+    (loop [row 1
+           summed-partitions []]
+      (cond
+        (= n 0) '(1)
+        (= n row)
+        (concat '(1) summed-partitions '(1))
+        :else (recur (inc row)
+               (map (partial apply +)
+                    (partition 2 1
+                               (concat '(1) summed-partitions '(1))))))))))
+(defn pascal-row [n]
+  (if (= n 0)
+    '(1)
+     (let [previous-row (pascal-row-aux (dec n))
+           sums (map (partial apply +) (partition 2 1 previous-row))]
+       (concat '(1) sums '(1)))))
+
+(defn app []
+  (let [active-n (r/atom nil)]
+    (fn []
+      [:div.app 
+       [:h1 "pascal's triangle"]
+       [:div.debug-panel (str @active-n)]
+       [:div.triangle
+        (doall
+         (for [i (range 50)]
+           ^{:key (str "row-" i)}
+           [:div.row
+            ((comp doall map-indexed)
+             (fn [j n]
+               ^{:key (str "row-" i "-col-" j)}
+               [:span.col
+                {:on-mouse-over (fn [] (reset! active-n {:row i :col j}))
+                 :class (cond
+                          (and (= i (:row @active-n)) (= j (:col @active-n)))
+                          "highlighted"
+                          (and (= i (dec (:row @active-n)))
+                               (#{j (inc j)} (:col @active-n)))
+                          "highlighted lighter")}
+
+                n])
+             (pascal-row i))]))]
+       [:style css-str]])))
+
+(defn mount-components! []
+  (render [app] app-root))
+
+(defn init! []
+  (mount-components!))
+
+(init!)
+(println "starting")
---
+++ src/main/pascal/styles.cljs
@@ -0,0 +1,40 @@
+(ns pascal.styles
+  (:require [garden.core :as gc :refer [css]]
+            [garden.selectors :as gs]
+            [garden.color :as color]))
+
+(gs/defclass row)
+(gs/defclass col)
+(gs/defclass debug-panel)
+(gs/defclass highlighted)
+(gs/defclass lighter)
+
+(def col-styles
+  [[(gs/selector col)
+    {:margin      :none
+     :border "1px solid lightgray"
+     :padding     "0.2rem 0.6rem"
+     :font-family "Playfair Display SC, serif"}]
+   
+   [(gs/selector row)
+    {:display         :flex
+     :justify-content :center}]
+   [(gs/selector debug-panel)
+    {:display          :block
+     :position         :fixed
+     :background-color :white
+     :top              "1em"
+     :padding          "1em"
+     :right            "1em"
+     :font-weight      :bold
+     :font-size        "1.3em"
+     :border-radius    "0.5em"
+     :opacity          0.7}]
+   [(gs/selector highlighted)
+    {:font-weight :bold
+     :color       :red}]
+   [(gs/& highlighted lighter)
+    {:color "#ff6666"}]])
+
+(def ^:export css-str (css col-styles))
+
